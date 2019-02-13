efc20b10ccfca566a14adbf271fd965355ffa8bb
dlm_global <dlmazz_global@163.com>
2019-02-13 11:56:04 +0800

    Handler和Binder机制总结

--- app/src/main/java/com/example/myandroidproject/MainActivity.java
+++ app/src/main/java/com/example/myandroidproject/MainActivity.java
@@ -3,10 +3,16 @@ package com.example.myandroidproject;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.IntentService;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.IBinder;
 import android.os.Message;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -28,7 +34,7 @@ import com.squareup.picasso.Picasso;
 import java.util.LinkedHashMap;
 import java.util.WeakHashMap;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     Handler workHandler;
     HandlerThread handlerThread;
@@ -128,6 +134,63 @@ public class MainActivity extends Activity {
     //RxJava
     public void rxjava(View view) {
 
+
         startActivity(new Intent(MainActivity.this, RXJavaActivity.class));
+    }
+
+    private void testAIDL(){
+
+
+        /**
+         * 1.C/S架构
+         * 2.Server向ServerManager注册自己，包括类名，方法名称的IBinder引用
+         * 3.Client从ServverManager中查询相应服务，绑定服务端，根据返回的Binder,new一个Proxy对象，然后调用相应的方法
+         * 4.通过序列化的方式，读写数据
+         *
+         */
+
+
+        Intent intent = new Intent(this,RemoteService.class);
+
+        this.bindService(intent, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                RemoteService.MyBinder binder = (RemoteService.MyBinder) IMyAidlInterface.Stub.asInterface(service);
+                try {
+                    binder.getName();
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+
+            }
+        },0);
+    }
+
+
+
+    class RemoteService extends Service{
+
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        class MyBinder extends IMyAidlInterface.Stub {
+
+            @Override
+            public String getName() throws RemoteException {
+                return null;
+            }
+
+            @Override
+            public void setName(String name) throws RemoteException {
+
+            }
+        }
     }
 }
